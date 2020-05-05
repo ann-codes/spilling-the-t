576fbe1a7ba050540a6fe5dfa482641f7dbe1deb
ule@wellesley.edu <ule@wellesley.edu>
2020-05-05 14:15:27 -0400

    Continue debugging 400s error

--- src/main/java/com/launchacademy/giantleap/controllers/api/v1/StationApiController.java
+++ src/main/java/com/launchacademy/giantleap/controllers/api/v1/StationApiController.java
@@ -1,17 +1,11 @@
 package com.launchacademy.giantleap.controllers.api.v1;
 
-import com.launchacademy.giantleap.dtos.UserDto;
 import com.launchacademy.giantleap.models.Station;
-import com.launchacademy.giantleap.models.User;
 import com.launchacademy.giantleap.repositories.StationRepository;
-import com.launchacademy.giantleap.repositories.UserRepository;
-import com.launchacademy.giantleap.services.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.PageRequest;
-import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.*;
 
@@ -34,11 +28,8 @@ public class StationApiController {
     }
 
     @PostMapping("/stations/new")
-    public ResponseEntity create(@Valid @RequestBody Station station, BindingResult bindingResult) {
-        if (bindingResult.hasErrors()) {
-            return new ResponseEntity<List>(bindingResult.getAllErrors(), HttpStatus.NOT_ACCEPTABLE);
-        } else {
-            return new ResponseEntity<Station>(stationRepository.save(station), HttpStatus.CREATED);
-        }
+    public String createStation(@ModelAttribute Station station, Model model) {
+        model.addAttribute(station);
+        return "trips/show";
     }
 }
