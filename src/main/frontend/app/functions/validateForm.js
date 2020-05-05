83bc000bb63e39cf0c0b24bff96877a7d04cdd30
ule@wellesley.edu <ule@wellesley.edu>
2020-05-05 17:00:18 -0400

    Removed commented out lines in validateForm.js file

--- src/main/frontend/app/functions/validateForm.js
+++ src/main/frontend/app/functions/validateForm.js
@@ -9,30 +9,6 @@ const validateForm = (requiredFields, stateGetter, errorSetter) => {
     }
   });
 
-//   const validPhoneNo = /^\(?([0-9]{3})\)?[-. ]?([0-9]{3})[-. ]?([0-9]{4})$/;
-//   if (
-//     !validPhoneNo.test(stateGetter["phoneNumber"]) &&
-//     stateGetter["phoneNumber"].trim() !== ""
-//   ) {
-//     submitErrors = {
-//       ...submitErrors,
-//       ["phoneNumber"]:
-//         "must be in valid format: xxx-xxx-xxxx, xxx.xxx.xxxx, xxx xxx xxxx",
-//     };
-//     console.log("is correct");
-//   }
-
-//   const validEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
-//   if (
-//     !validEmail.test(stateGetter["email"]) &&
-//     stateGetter["email"].trim() !== ""
-//   ) {
-//     submitErrors = {
-//       ...submitErrors,
-//       ["email"]: "must be in valid format.",
-//     };
-//   }
-
   errorSetter(submitErrors);
   return (
     Object.entries(submitErrors).length === 0 &&
