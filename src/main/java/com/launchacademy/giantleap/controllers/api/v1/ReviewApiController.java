b5cf4da12f9249099f96067902e9da174f7f9379
ule@wellesley.edu <ule@wellesley.edu>
2020-05-07 14:49:22 -0400

    Added GET mappings to ReviewAPIController plus extra seeds for testing

--- src/main/java/com/launchacademy/giantleap/controllers/api/v1/ReviewApiController.java
+++ src/main/java/com/launchacademy/giantleap/controllers/api/v1/ReviewApiController.java
@@ -26,7 +26,8 @@ public class ReviewApiController {
     }
 
     @NoArgsConstructor
-    private class NotFoundException extends RuntimeException {}
+    private class NotFoundException extends RuntimeException {
+    }
 
     @ControllerAdvice
     private class NotFoundAdvice {
@@ -42,14 +43,9 @@ public class ReviewApiController {
     public Iterable<Review> getAllReviews() {
         return reviewRepository.findAll();
     }
-<<<<<<< HEAD
 
     @GetMapping("review/{id}")
     public Optional<Review> getOneReview(@PathVariable Integer id) {
         return reviewRepository.findById(id);
     }
-
-=======
-    
->>>>>>> 3e428f89b991db2c010bdd57c2683815172bef2d
 }
