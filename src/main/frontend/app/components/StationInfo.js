1b740f761be8cf84d33cd190906b14a9d2b98f8e
Ann <ann.nguyen.codes@gmail.com>
2020-05-08 12:27:10 -0400

    file name changes

--- src/main/frontend/app/components/StationInfo.js
+++ src/main/frontend/app/components/StationInfo.js
@@ -1,20 +1,20 @@
-import React from "react"
+import React from "react";
 
 const StationInfo = (props) => {
-	return (
-		<div className="text-center">
-			<h2>{props.station.name}</h2>
-			<img className="img-tweaks" src={props.station.imageUrl} />
-			<p className="lead bold-me">
-				{props.station.name} is {props.station.lineName}
-				{props.station.address}
-			</p>
-			<p className="lead">
-				<span className="bold-me">My State: </span>
-				{props.station.state}
-			</p>
-		</div>
-	)
-}
+  return (
+    <div className="text-center">
+      <h2>{props.station.name}</h2>
+      <img className="img-tweaks" src={props.station.imageUrl} />
+      <p className="lead bold-me">
+        {props.station.name} is {props.station.lineName}
+        {props.station.address}
+      </p>
+      <p className="lead">
+        <span className="bold-me">My State: </span>
+        {props.station.state}
+      </p>
+    </div>
+  );
+};
 
-export default StationInfo
+export default StationInfo;
