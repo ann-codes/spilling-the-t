e531012513731b4d585139e63d07da274f579ecf
Ann <ann.nguyen.codes@gmail.com>
2020-05-13 17:50:04 -0400

    login auth now works

--- src/main/frontend/app/containers/Login.js
+++ src/main/frontend/app/containers/Login.js
@@ -1,9 +1,11 @@
-import React, { Fragment } from "react";
+import React, { useState, useEffect, Fragment } from "react";
 import { useCookies } from "react-cookie";
 import LoginForm from "../components/LoginForm";
 
 const Login = (props) => {
-  const [cookies, setCookie, removeCookie] = useCookies(["SPTlogin"]);
+  const [cookies, setCookie] = useCookies(["SPTlogin"]);
+  const [loginForm, setLoginForm] = useState({ username: "", password: "" });
+  const [errors, setErrors] = useState({});
   const cookieExist = cookies.SPTlogin !== undefined;
   let username = "";
 
@@ -11,10 +13,45 @@ const Login = (props) => {
     username = <span> as {cookies.SPTlogin.username}</span>;
   }
 
+  const authOnSubmit = (payload) => {
+    return fetch(`/api/v1/auth/${payload.username}/${payload.password}`, {
+      headers: {
+        "Content-Type": "application/json",
+        credentials: "same-origin",
+      },
+    })
+      .then((response) => {
+        if (response.ok) {
+          return response;
+        } else {
+          throw new Error(`${response.status} (${response.statusText})`);
+        }
+      })
+      .then((response) => response.json())
+      .then((body) => {
+        if (body.length > 0) {
+          setCookie("SPTlogin", body[0], { path: "/" });
+        }
+        if (body.length === 0) {
+          setErrors({
+            ...errors,
+            ["password"]: "and username combination does not match",
+          });
+        }
+      })
+      .catch((error) => console.error(`Error in fetch: ${error.message}`));
+  };
+
   return (
     <Fragment>
       {!cookieExist ? (
-        <LoginForm />
+        <LoginForm
+          loginForm={loginForm}
+          errors={errors}
+          setErrors={setErrors}
+          setLoginForm={setLoginForm}
+          authOnSubmit={authOnSubmit}
+        />
       ) : (
         <div className="box">
           <h1>You are now logged in{username}</h1>
