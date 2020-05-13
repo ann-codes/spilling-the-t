17ca19a44c179b33d1044291109beb039fb22859
Ann <ann.nguyen.codes@gmail.com>
2020-05-13 12:13:36 -0400

    indent

--- src/main/frontend/app/components/StationInfo.js
+++ src/main/frontend/app/components/StationInfo.js
@@ -14,12 +14,19 @@ const StationInfo = (props) => {
     <div className="box">
       <h2>{props.station.name} Station</h2>
       <img src={props.station.imageUrl} />
-	  <p><strong>Network:</strong> {props.station.network}</p>
-      <p><strong>Line:</strong> {props.station.lineName} Line</p>
-      <p><strong>Address:</strong> <br />
-        {props.station.name} Station<br />
-		{props.station.address}<br/>
-		{props.station.city}, {props.station.state} {props.station.zip}
+      <p>
+        <strong>Network:</strong> {props.station.network}
+      </p>
+      <p>
+        <strong>Line:</strong> {props.station.lineName} Line
+      </p>
+      <p>
+        <strong>Address:</strong> <br />
+        {props.station.name} Station
+        <br />
+        {props.station.address}
+        <br />
+        {props.station.city}, {props.station.state} {props.station.zip}
       </p>
       <h2>{props.station.name} Station Reviews</h2>
       {mapReviews}
